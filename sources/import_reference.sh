e2300a118108ec05620c51c1c901c5cc1218687d
Thibaut Montagne <thibaut@montagne.pw>
2017-01-18 20:25:12 +0100

    Update sources/import_reference.sh

--- sources/import_reference.sh
+++ sources/import_reference.sh
@@ -10,6 +10,7 @@ function USAGE() {
 	printf "\t%s -h\n\n" $( basename ${0} )
 	echo -e "Required arguments :"
 	echo -e "\t-d xxx"
+	echo -e "\t-o xxx"
 	echo -e "\t-f xxx"
 	echo -e "\t-g xxx"
 	echo
@@ -24,6 +25,7 @@ function USAGE() {
 #####################
 
 DATABASE="NOTDEFINED"
+OUTDIR="NOTDEFINED"
 FASTA_INFO_PATH="NOTDEFINED"
 GFF_INFO_PATH="NOTDEFINED"
 VERBOSE="NO"
@@ -32,7 +34,7 @@ VERBOSE="NO"
 # OPTION PARSING #
 ##################
 
-while getopts ":vhd:f:g:" OPTION
+while getopts ":vhd:f:g:o:" OPTION
 do
 	case ${OPTION} in
 		d)
@@ -49,6 +51,13 @@ do
 			GFF_INFO_FULLNAME=$( basename ${GFF_INFO_PATH} )
 			GFF_INFO_NAME=${GFF_INFO_FULLNAME%%.*}
 			GFF_INFO_EXTENSION=${GFF_INFO_FULLNAME#*.}
+			;;
+		o)
+			OUTDIR=${OPTARG}
+			mkdir -p ${OUTDIR}
+			#-p option
+				# Create intermediate directories as required.
+				# With this option specified, no error will be reported if a directory given as an operand already exists.
 			;;
 		v)
 			VERBOSE="YES"
@@ -79,15 +88,24 @@ echo -e "\n#############################\n# BEGIN $( basename ${0} ) #\n########
 # VERIFICATIONS #
 #################
 
-if [[ ${DATABASE} == "NOTDEFINED" || ${FASTA_INFO_PATH} == "NOTDEFINED" || ${GFF_INFO_PATH} == "NOTDEFINED" ]] ; then
+if [[ ${DATABASE} == "NOTDEFINED" || ${FASTA_INFO_PATH} == "NOTDEFINED" || ${GFF_INFO_PATH} == "NOTDEFINED" || ${OUTDIR} == "NOTDEFINED" ]] ; then
 	USAGE
 	exit 1
 fi
 
 ################################################################################
 
-head ${FASTA_INFO_PATH}
-head ${GFF_INFO_PATH}
+cp import_reference_template.sql ${OUTDIR}/import_reference.sql
+
+replace "DB_NAME"         "${DATABASE}"          -- ${OUTDIR}/import_reference.sql
+replace "INFO_FROM_FASTA" "'${FASTA_INFO_PATH}'" -- ${OUTDIR}/import_reference.sql
+replace "INFO_FROM_GFF"   "'${GFF_INFO_PATH}'"   -- ${OUTDIR}/import_reference.sql
+
+################################################################################
+
+mysql.server start
+mysql --user=root --password --local-infile < ${OUTDIR}/import_reference.sql
+mysql.server stop
 
 ################################################################################
 
@@ -95,4 +113,4 @@ head ${GFF_INFO_PATH}
 # END SCRIPT #
 ##############
 
-echo -e "\n###########################\n# END $( basename ${0} ) #\n###########################\n\n"
+echo -e "\n###########################\n# END $( basename ${0} ) #\n###########################\n"
