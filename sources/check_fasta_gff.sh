a7cabeb36efbb241460dff90ea88247d593168b6
Thibaut Montagne <thibaut@montagne.pw>
2017-02-02 01:20:23 +0100

    Update check_fasta_gff.sh

--- sources/check_fasta_gff.sh
+++ sources/check_fasta_gff.sh
@@ -7,25 +7,23 @@ set -u
 
 function USAGE() {
 
-	echo -e "\n############################\n# BEGIN $( basename ${0} ) #\n############################\n"
-
-	printf "Usage:\n\t%s -f FASTA_FILE -g GFF_FILE -o OUTDIR [-m MIN_SEQ_LENGTH] [-c] [-v]\n" $( basename ${0} )
-	printf "\t%s [-h]\n\n" $( basename ${0} )
+	printf "Usage:\n\t%s -f FASTA_FILE -g GFF_FILE [-c] [-m MIN_SEQ_LENGTH] [-o OUTDIR] [-v]\n" $( basename ${0} )
+	printf "\t%s [-h]\n" $( basename ${0} )
 
 	cat <<EOF
 
-	Required options:
-		-f : FASTA file
-		-g : GFF file
-		-o : output directory
+Required arguments:
+	-f : FASTA file
+	-g : GFF file
 
-	Optionnal options:
-		-m : minimum sequence length        [by default : 1]"
-		-c : if files are compressed (gzip) [by default : not compressed]"
+Optional arguments:
+	-c : if files are compressed (gzip) [by default: not compressed]
+	-m : minimum sequence length        [by default: 1]
+	-o : output directory               [by default: working directory]
 
-	Help:
-		-v : to activate verbose mode       [by default : not activated]"
-		-h : to print this help
+Help:
+	-h : to print this help
+	-v : to activate VERBOSE       [by default: not activated]
 
 Description:
 
@@ -39,8 +37,6 @@ Description:
 	- discard 'too short' seqids based on -m option
 
 EOF
-
-	echo -e "\n##########################\n# END $( basename ${0} ) #\n##########################\n"
 
 	exit ${1:-0}
 
@@ -120,7 +116,7 @@ fi
 # BEGIN SCRIPT #
 ################
 
-echo -e "\n############################\n# BEGIN $( basename ${0} ) #\n############################\n"
+echo -e "\n# BEGIN $( basename ${0} )"
 
 ################################################################################
 
@@ -143,46 +139,44 @@ fi
 
 ################################################################################
 
-# If bash_bioinfo already cloned : remove it and clone it again to get the most recent one from Github
+# If bash_bioinfo already cloned : remove it
 if [[ -d "bash_bioinfo" ]] ; then
+
+	# Remove bash_bioinfo/ directory
 	CMD1="rm -rf bash_bioinfo/"
-	CMD3="source 'bash_bioinfo/.bash_bioinfo'"
-	CMD2="git clone 'https://github.com/thbtmntgn/bash_bioinfo' 2> /dev/null"
-	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Remove bash_bioinfo/ directory\n\t---> Commande : ${CMD1}\n" ; fi
+	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD1}" ; fi
 	eval ${CMD1}
-	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Get .bash_bioinfo from Github\n\t---> Commande : ${CMD2}\n" ; fi
-	eval ${CMD2}
-	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Source .bash_bioinfo\n\t---> Commande : ${CMD3}\n" ; fi
-	eval ${CMD3}
-else
-	CMD1="git clone 'https://github.com/thbtmntgn/bash_bioinfo' 2> /dev/null"
-	CMD2="source 'bash_bioinfo/.bash_bioinfo'"
-	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Get .bash_bioinfo from Github\n\t---> Commande : ${CMD1}\n" ; fi
-	eval ${CMD1}
-	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Source .bash_bioinfo\n\t---> Commande : ${CMD2}\n" ; fi
-	eval ${CMD2}
+
 fi
+
+# Get .bash_bioinfo from Github
+CMD2="git clone 'https://github.com/thbtmntgn/bash_bioinfo' 2> /dev/null"
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD2}" ; fi
+eval ${CMD2}
+
+# Source .bash_bioinfo
+CMD3="source 'bash_bioinfo/.bash_bioinfo'"
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD3}" ; fi
+eval ${CMD3}
 
 ################################################################################
 
 # Verbose prints
-if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Your GFF file is           : ${GFF_FULLNAME}"   ; fi
-if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Your FASTA file is         : ${FASTA_FULLNAME}" ; fi
-if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Your output directory is   : ${OUTDIR}\n"       ; fi
-if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Minimal sequence length is : ${MIN_LENGTH}"     ; fi
-if [[ ${VERBOSE} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> Verbose mode is activated"                      ; fi
 if [[ ${VERBOSE} == "YES" ]] ; then
-	if [[ ${COMPRESSED} == "YES" ]] ; then echo -e "[VERBOSE MODE]---> FASTA and GFF files are compressed"      ; fi
-	if [[ ${COMPRESSED} == "NO" ]]  ; then echo -e "[VERBOSE MODE]---> FASTA and GFF files are not compressed"  ; fi
+	echo -e "\t[VERBOSE INFO] ---> Your GFF file is           : ${GFF_FULLNAME}"
+	echo -e "\t[VERBOSE INFO] ---> Your FASTA file is         : ${FASTA_FULLNAME}"
+	echo -e "\t[VERBOSE INFO] ---> Your output directory is   : ${OUTDIR}"
+	echo -e "\t[VERBOSE INFO] ---> Minimal sequence length is : ${MIN_LENGTH}"
+	echo -e "\t[VERBOSE INFO] ---> VERBOSE is activated"
+	if [[ ${COMPRESSED} == "YES" ]] ; then echo -e "\t[VERBOSE INFO] ---> FASTA and GFF files are compressed"       ; fi
+	if [[ ${COMPRESSED} == "NO" ]]  ; then echo -e "\t[VERBOSE INFO] ---> FASTA and GFF files are not compressed" ; fi
 fi
 
 ################################################################################
 
 # Get sequence ID + sequence length from FASTA file
 CMD="fadesc ${FASTA} > ${OUTDIR}/${FASTA_NAME}.desc"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Get sequence ID + sequence length from FASTA file"
-	echo -e "\t---> Commande : ${CMD}\n" ; fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
@@ -190,12 +184,10 @@ eval ${CMD}
 # Compare and filter seqids from FASTA and GFF
 if [[ ${VERBOSE} == "YES" ]] ; then
 	CMD="perl check_fasta_gff.pl --description ${OUTDIR}/${FASTA_NAME}.desc --gff ${GFF} --outdir ${OUTDIR} --verbose"
-	echo -e "[VERBOSE MODE]---> Compare and filter seqids from FASTA and GFF"
-	echo -e "\t---> Commande : ${CMD}\n"
+	echo -e "\t[VERBOSE COMMAND] ---> ${CMD}"
 else
 	CMD="perl check_fasta_gff.pl --description ${OUTDIR}/${FASTA_NAME}.desc --gff ${GFF} --outdir ${OUTDIR}"
 fi
-
 eval ${CMD}
 
 ################################################################################
@@ -204,10 +196,7 @@ eval ${CMD}
 if [[ -e "${OUTDIR}/seqids_in_common_concordant.desc" ]] ; then
 
 	CMD="cat ${OUTDIR}/seqids_in_common_concordant.desc | sort -nr -k2 > ${OUTDIR}/seqids_in_common_concordant_sorted.desc"
-	if [[ ${VERBOSE} == "YES" ]] ; then
-		echo -e "[VERBOSE MODE]---> If common seqids (with concordant length between FASTA and GFF) are retrieved, they are sorted by FASTA sequence length (longest to smallest)"
-		echo -e "\t---> Commande : ${CMD}\n"
-	fi
+	if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 	eval ${CMD}
 
 else
@@ -221,20 +210,14 @@ fi
 
 # Get seqids from LIST file
 CMD="cat ${OUTDIR}/seqids_in_common_concordant_sorted.desc | awk '{ if (\$2 >= ${MIN_LENGTH}){ print \$0 } }' > ${OUTDIR}/filtered_seqids.desc"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Get seqids with length equal or greater than ${MIN_LENGTH} pb"
-	echo -e "\t---> Commande : ${CMD}\n"
-fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
 
 # Get too short SEQIDS
 CMD="cat ${OUTDIR}/seqids_in_common_concordant_sorted.desc | awk '{ if (\$2 < ${MIN_LENGTH}){ print \$0 } }' > ${OUTDIR}/seqids_in_common_concordant_too_short.desc"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Get seqids with length lower than ${MIN_LENGTH} pb"
-	echo -e "\t---> Commande : ${CMD}\n"
-fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
@@ -242,9 +225,12 @@ eval ${CMD}
 SEQIDS_COMMON_NUM=$( cat ${OUTDIR}/seqids_in_common_concordant_sorted.desc | wc -l | awk '{print $1}' )
 SEQIDS_TOO_SHORT_NUM=$( cut -f1 ${OUTDIR}/seqids_in_common_concordant_too_short.desc | wc -l | awk '{print $1}' )
 SEQIDS_FILTERED_NUM=$( cut -f1 ${OUTDIR}/filtered_seqids.desc | wc -l | awk '{print $1}' )
-echo -e "---> Concordant seqids           : ${SEQIDS_COMMON_NUM}"
-echo -e "---> Concordant seqids too short : ${SEQIDS_TOO_SHORT_NUM}"
-echo -e "---> Keeped seqids               : ${SEQIDS_FILTERED_NUM}\n"
+
+if [[ ${VERBOSE} == "YES" ]] ; then
+	echo -e "\t[VERBOSE INFO] ---> Concordant seqids           : ${SEQIDS_COMMON_NUM}"
+	echo -e "\t[VERBOSE INFO] ---> Concordant seqids too short : ${SEQIDS_TOO_SHORT_NUM}"
+	echo -e "\t[VERBOSE INFO] ---> Keeped seqids               : ${SEQIDS_FILTERED_NUM}"
+fi
 
 ################################################################################
 
@@ -256,58 +242,50 @@ fi
 
 # Index FASTA file
 CMD="samtools faidx ${FASTA}"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Index FASTA file"
-	echo -e "\t---> Commande : ${CMD}\n"
-fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
 
 # Get only seqid field
 CMD="cut -f1 ${OUTDIR}/filtered_seqids.desc > ${OUTDIR}/filtered_seqids.list"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Get only seqid field"
-	echo -e "\t---> Commande : ${CMD}\n"
-fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
 
 # Get FASTA sequences with seqids in LIST
 CMD="faget ${FASTA} ${OUTDIR}/filtered_seqids.list > ${OUTDIR}/filtered_${FASTA_NAME}.${FASTA_EXTENSION}"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Get FASTA sequences with seqids in ${OUTDIR}/filtered_seqids.list"
-	echo -e "\t---> Commande : ${CMD}\n"
-fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
 
 # Get GFF sequences with seqids in LIST
 CMD="grep -F -w -f ${OUTDIR}/filtered_seqids.list ${GFF} > ${OUTDIR}/filtered_${GFF_NAME}.${GFF_EXTENSION}"
-if [[ ${VERBOSE} == "YES" ]] ; then
-	echo -e "[VERBOSE MODE]---> Get GFF sequences with seqids in ${OUTDIR}/filtered_seqids.list"
-	echo -e "\t---> Commande : ${CMD}\n"
-fi
+if [[ ${VERBOSE} == "YES" ]] ; then echo -e "\t[VERBOSE COMMAND] ---> ${CMD}" ; fi
 eval ${CMD}
 
 ################################################################################
 # Check seqids number in LIST, new FASTA and new GFF
 
-FASTA_SEQID_NUM=$( grep -c ">" ${OUTDIR}/filtered_${FASTA_NAME}.${FASTA_EXTENSION} )
-GFF_SEQID_NUM=$( grep -v "^#" ${OUTDIR}/filtered_${GFF_NAME}.${GFF_EXTENSION} | cut -f1 | sort -u | wc -l | awk '{ print $1 }' )
+if [[ ${VERBOSE} == "YES" ]] ; then
 
-if [[ ${SEQIDS_FILTERED_NUM} -eq ${FASTA_SEQID_NUM} && ${SEQIDS_FILTERED_NUM} -eq ${GFF_SEQID_NUM} ]] ; then
-	echo -e "---> ALL GOOD !"
-	echo -e "\t---> Keeped seqids at the end : ${SEQIDS_FILTERED_NUM}"
-	echo -e "\t---> Seqids in final fasta    : ${FASTA_SEQID_NUM}"
-	echo -e "\t---> Seqids in final gff      : ${GFF_SEQID_NUM}"
-else
-	echo -e "---> PROBLEM ! !"
-	echo -e "\t---> Keeped seqids at the end : ${SEQIDS_FILTERED_NUM}"
-	echo -e "\t---> Seqids in final fasta    : ${FASTA_SEQID_NUM}"
-	echo -e "\t---> Seqids in final gff      : ${GFF_SEQID_NUM}"
+	FASTA_SEQID_NUM=$( grep -c ">" ${OUTDIR}/filtered_${FASTA_NAME}.${FASTA_EXTENSION} )
+	GFF_SEQID_NUM=$( grep -v "^#" ${OUTDIR}/filtered_${GFF_NAME}.${GFF_EXTENSION} | cut -f1 | sort -u | wc -l | awk '{ print $1 }' )
+
+	if [[ ${SEQIDS_FILTERED_NUM} -eq ${FASTA_SEQID_NUM} && ${SEQIDS_FILTERED_NUM} -eq ${GFF_SEQID_NUM} ]] ; then
+		echo -e "\t[VERBOSE INFO] ---> ALL GOOD !"
+		echo -e "\t[VERBOSE INFO] ---> Keeped seqids at the end : ${SEQIDS_FILTERED_NUM}"
+		echo -e "\t[VERBOSE INFO] ---> Seqids in final fasta    : ${FASTA_SEQID_NUM}"
+		echo -e "\t[VERBOSE INFO] ---> Seqids in final gff      : ${GFF_SEQID_NUM}"
+	else
+		echo -e "\t[VERBOSE INFO] ---> PROBLEM ! !"
+		echo -e "\t[VERBOSE INFO] ---> Keeped seqids at the end : ${SEQIDS_FILTERED_NUM}"
+		echo -e "\t[VERBOSE INFO] ---> Seqids in final fasta    : ${FASTA_SEQID_NUM}"
+		echo -e "\t[VERBOSE INFO] ---> Seqids in final gff      : ${GFF_SEQID_NUM}"
+	fi
+
 fi
 
 ################################################################################
@@ -324,4 +302,4 @@ rm ${OUTDIR}/${FASTA_NAME}.desc
 # END SCRIPT #
 ##############
 
-echo -e "\n##########################\n# END $( basename ${0} ) #\n##########################\n"
+echo -e "# END $( basename ${0} )\n"
